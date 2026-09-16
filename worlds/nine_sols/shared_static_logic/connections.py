a85d1eb9547046b8102b1c60af0a3cb139cc9492
a1s4aaron1 <a1s4aaron@yahoo.com>
2026-09-16 11:08:44 -0700

    Correcting syntax
    I don't really know the reason it is failing so I'm seeing if these spaces change it.

--- worlds/nine_sols/shared_static_logic/connections.py
+++ worlds/nine_sols/shared_static_logic/connections.py
@@ -1584,15 +1584,15 @@ connections_data = [
             { "item": "Air Dash" },
             { "item": "Grapple" }
         ], # this is *possible* with CL (AND grapple) instead, but very annoying
-        "medium_requires":[ 
+        "medium_requires": [ 
              { "item": "Grapple" } # T-dash replaces air dash, you obtain qi from enemy in bottom right
         ], 
-        "ls_requires":[ 
+        "ls_requires": [ 
             # Wall slide up with CL then use CL, T-dash, air dash, and either CS or SR to skip the grapple line
             { "item": "Air Dash" }, 
             { "item": "Cloud Leap" }, 
             { "item": "Ledge Grab" }, 
-            { "anyof":[ { "item": "Charged Strike" }, { "item": "Trick - Long Jump with Swift Runner" } ] }
+            { "anyof": [ { "item": "Charged Strike" }, { "item": "Trick - Long Jump with Swift Runner" } ] }
         ]
     }, 
     {
